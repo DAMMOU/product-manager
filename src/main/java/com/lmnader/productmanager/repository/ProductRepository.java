eb335b6df662812e4611a96db90d443097dee1be
DAMMOU <udammou@gmail.com>
2023-10-22 16:20:12 +0200

    Add repository tests for product management features

--- src/main/java/com/lmnader/productmanager/repository/ProductRepository.java
+++ src/main/java/com/lmnader/productmanager/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.lmnader.productmanager.repository;
 
 import com.lmnader.productmanager.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
--- src/test/java/com/lmnader/productmanager/ProductManagerApplicationTests.java
+++ src/test/java/com/lmnader/productmanager/ProductManagerApplicationTests.java
@@ -1,13 +1,51 @@
 package com.lmnader.productmanager;
 
+import com.lmnader.productmanager.model.Product;
+import com.lmnader.productmanager.repository.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.AssertionErrors;
+
+import java.util.Date;
 
 @SpringBootTest
 class ProductManagerApplicationTests {
 
+	@Autowired
+	private ProductRepository productRepository;
 	@Test
-	void contextLoads() {
+	public void createProdcutTest(){
+		Product product = new Product();
+		product.setName("ggg");
+		product.setDescription("fksfv");
+		product.setPrice(2000.00);
+		product.setCreationDate(new Date());
+		productRepository.save(product);
 	}
+	@Test
+	public void testFindProductById() {
+		Long productId = 1L;
+		Product product = productRepository.findById(productId).orElse(null);
+		// Vérifier que le produit a été trouvé
+		System.out.println(product);
+	}
+	@Test
+	public void updateProductTest() {
+		Long productId = 1L;
+		try {
+			Product product = productRepository.findById(productId).orElse(null);
+			if (product != null) {
+				product.setPrice(10.0);
+				productRepository.save(product);
+			} else {
+				System.out.println("Id = " + productId + " not found");
+			}
+		} catch (Exception e) {
+			System.out.println("Error: " + e);
+		}
+	}
+
 
 }
